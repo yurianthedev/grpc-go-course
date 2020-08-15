5bca6a26bacbda938f3bd518bce75ca3ff5827a5
yurianxdev <bryangarzon22@gmail.com>
2020-08-14 21:31:40 -0500

    Client implementation of Response streaming gRPC on Calculator service

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"google.golang.org/grpc"
@@ -22,14 +23,40 @@ func main() {
 	c := calculatorpb.NewCalculatorClient(conn)
 
 	doUnary(c)
+
+	doServerStreaming(c)
+}
+
+func doServerStreaming(c calculatorpb.CalculatorClient) {
+	log.Printf("Starting streaming...\n")
+	req := &calculatorpb.PrimeNumberDecompositionRequest{
+		Number: 120,
+	}
+	resStream, err := c.PrimeNumberDecomposition(context.Background(), req)
+	if err != nil {
+		log.Printf("Something failed in the streaming: %v\n", err)
+		return
+	}
+
+	for {
+		msg, err := resStream.Recv()
+		if err == io.EOF {
+			log.Printf("End of the stream\n")
+			break
+		}
+		if err != nil {
+			log.Printf("Error reading stream: %v\n", err)
+		}
+		fmt.Printf("Response from PrimeNumberDecomposition: %d\n", msg.GetPrimeNumber())
+	}
 }
 
 func doUnary(c calculatorpb.CalculatorClient) {
 	req := &calculatorpb.CalculatorRequest{NumberOne: 1, NumberTwo: 2}
 	res, err := c.Sum(context.Background(), req)
 	if err != nil {
-		log.Fatalf("Error requesting for a Sum: %v", err)
+		log.Fatalf("Error requesting for a Sum: %v\n", err)
 	}
 
-	fmt.Printf("The sum of those two numbers is: %d", res.Result)
+	fmt.Printf("The sum of those two numbers is: %d\n", res.Result)
 }
