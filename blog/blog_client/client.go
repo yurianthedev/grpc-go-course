45df157655a72bce8cfb57542a1bbf5dd0062cd6
yurianxdev <bryangarzon22@gmail.com>
2020-08-18 01:52:33 -0500

    Client for DeleteBlog

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -22,11 +22,52 @@ func main() {
 	log.Println("Successfully dial with RPC server on port 50051")
 
 	c := blogpb.NewBlogServiceClient(conn)
-	createBlog(c)
-	readBlog(c, "somerandomid")
-	readBlog(c, "5f3b6340b0039109d78d521e")
-	updateBlog(c, "5f3b6340b0039109d78d521e")
-	readBlog(c, "5f3b6340b0039109d78d521e")
+	id := createBlog(c, &blogpb.Blog{
+		AuthorId: "Julian",
+		Title:    "My first blog",
+		Content:  "Some content",
+	})
+	readBlog(c, "SomeRandomId") // This will fail
+	readBlog(c, id)             // Found blog created just before
+	updateBlog(c, id)
+	readBlog(c, id)
+	deleteBlog(c, id) // Delete the only blog
+
+	// Create some record for testing
+	createBlog(c, &blogpb.Blog{
+		AuthorId: "Julian",
+		Title:    "Some blog",
+		Content:  "Some content",
+	})
+	createBlog(c, &blogpb.Blog{
+		AuthorId: "Bryan",
+		Title:    "My third blog",
+		Content:  "Some content",
+	})
+	createBlog(c, &blogpb.Blog{
+		AuthorId: "Rincon",
+		Title:    "Blog",
+		Content:  "Some content",
+	})
+	createBlog(c, &blogpb.Blog{
+		AuthorId: "Garzon",
+		Title:    "Another blog",
+		Content:  "Some content",
+	})
+}
+
+func deleteBlog(c blogpb.BlogServiceClient, id string) {
+	log.Println("Calling DeleteBlog RPC...")
+
+	deleteRes, err := c.DeleteBlog(context.Background(), &blogpb.DeleteBlogRequest{
+		BlogId: id,
+	})
+	if err != nil {
+		log.Printf("Error deleting blog, %v\n", err)
+		return
+	}
+
+	fmt.Printf("Blog deleted: %s\n", deleteRes.GetBlogId())
 }
 
 func updateBlog(c blogpb.BlogServiceClient, id string) {
@@ -42,6 +83,7 @@ func updateBlog(c blogpb.BlogServiceClient, id string) {
 	})
 	if err != nil {
 		log.Printf("Error updating blog, %v\n", err)
+		return
 	}
 
 	fmt.Printf("Updated Blog: %v\n", res.GetBlog())
@@ -60,21 +102,17 @@ func readBlog(c blogpb.BlogServiceClient, id string) {
 	fmt.Printf("Blog found: %v\n", res.GetBlog())
 }
 
-func createBlog(c blogpb.BlogServiceClient) {
+func createBlog(c blogpb.BlogServiceClient, blog *blogpb.Blog) string {
 	log.Println("Calling CreateBlog RPC...")
 
-	blog := &blogpb.Blog{
-		AuthorId: "Julian",
-		Title:    "My first blog",
-		Content:  "Some content",
-	}
 	res, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{
 		Blog: blog,
 	})
 	if err != nil {
 		log.Printf("Error at CreateBlog RPC: %v\n", err)
-		return
+		return ""
 	}
 
 	fmt.Printf("Blog created: %v\n", res.GetBlog())
+	return res.GetBlog().GetId()
 }
